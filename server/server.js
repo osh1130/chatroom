2632c3bcf5e8540b4f79610eba237d3b6b4b1d84
Vivi <vivizhao401@gmail.com>
2022-12-05 18:01:23 +0800

    Server/Client Deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const expressWs = require("express-ws")
 const app = express();
-const port = 3000;
+const port = 3030;
+const websocket = require('./websocket')
 expressWs(app)
 
 // 中间件
@@ -10,6 +11,7 @@ app.use('/dufu', express.static('public/dufu.html'))
 app.use('/master', express.static('public/master.html'))
 app.use('/visitor', express.static('public/visitor.html'))
 app.use(express.static('public'))	// 主动访问public下的文件
+app.use('/ws', websocket)		// 访问/ws路径时，调用websocket
 app.get("/*", (req,res)=>{})
 app.listen(port, ()=>{
   console.log(`Server is running at http://localhost:${port}`)
--- server/websocket.js
+++ server/websocket.js
@@ -3,14 +3,40 @@ const expressWs = require("express-ws")
 const router = express.Router()
 expressWs(router)
 
-// 李四访问的地址
-router.ws("/lisi", ws=>{
-  ws.send("李四连接上了")
+// 声明一个数组，用来存放李四发出来的信息
+let libaiArr = [];
+
+// libai访问的地址
+router.ws("/libai", ws=>{
+  //ws.send("libai连接上了")
+  ws.on('message', msg=>{
+    // 接收李四传来的信息，存储后，再返回给李四
+    libaiArr.push(msg);
+    ws.send(msg);
+})
 })
 
-// 王五访问的地址
-router.ws("/wangwu", ws=>{
-  ws.send("王五连接上了")
+
+// dufu访问的地址
+router.ws("/dufu", ws=>{
+  ws.send("dufu连接上了")
+})
+
+//dufu接受数据
+router.ws('/dufu1', ws=>{
+  let timer = null;
+  // 监听到连接断开时，停止定时器，避免性能损耗
+  ws.on('close', ()=>{
+      if(timer){ clearInterval(timer) }
+  })
+  timer = setInterval(()=>{
+    // 每一秒检查一次lisiArr
+    if(libaiArr.length>0){
+      let m = libaiArr[0];	// 获取第一项
+      libaiArr.splice(0, 1);	// 删除第一项
+      ws.send(m);		// send方法会阻断后续代码，因此必须写在最后
+    }
+  }, 1000)
 })
 
 module.exports = router;
